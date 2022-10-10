0d58a7db9ee805549f7daead2c28c7b17f512d5e
AmberbirMehabaw <mehabawamberbir@gmail.com>
2022-10-10 16:21:48 +0300

    some changes

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
+
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlite((configuration.GetConnectionString("DefaultConnection"))));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
